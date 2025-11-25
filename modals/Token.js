5466cfecd7fa595069ccc0635394902251988c2b
Ritesh Kumar Singh <93989204+riteshsinghh-coder@users.noreply.github.com>
2025-11-25 23:23:57 +0530

    Delete modals directory

--- modals/Token.js
+++
@@ -1,4 +0,0 @@
-PORT=3000
-MONGO_URI=mongodb+srv://Catination:Catination@cluster0.ptwav98.mongodb.net/?appName=Cluster0
-SSE_URL=https://api.catination.com/service/notifications/stream?tenantId=29ABCDE1234F2Z5&streamKey=HelloAryan
-FIREBASE_SERVICE_ACCOUNT_PATH=serviceAccountKey.json
